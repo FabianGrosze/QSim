892045ec7a64007a0b807d2fda8f364f4090e3ec
wyrwa <wyrwa@bafg.de>
2020-01-08 13:19:30 +0100

    git und Visual Studio, erstmal ohne

--- qsim1d/qsim.f90
+++ qsim1d/qsim.f90
@@ -4,6 +4,7 @@
                                                                        
 !      izdt Einheiten min oder Stunden Beruecksichtigung bei itime      
 !      Bei Tracerrechnung wird für die Variable tempw mit der Tracermenge belegt!!!
+!      jetzt ins git
 !.....testen: WEHRO2                                                    
 
       character                               :: ckenn,cpoint,CST_end 
