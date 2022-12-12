1c1a81c2dd3402630495ae19874f2c5131e6a143
Michael Schoenung <schoenung@bafg.de>
2022-12-12 13:17:49 +0100

    Restyle lin_interpolation.f90

--- qsim1d/lin_interpolation.f90
+++ qsim1d/lin_interpolation.f90
@@ -26,27 +26,44 @@
 ! --------------------------------------------------------------------------- !
 
 !> Bestimmung der neuen Gitterwerte durch lineare Interpolation
-subroutine lin_interpolation(y_var,y_var_neu,hctiefe_neu,tiefe_neu,nkzs_alt, &
-                             nkzs_neu,i_zaehlv,itags,monats,mstr,ior)
+subroutine lin_interpolation(y_var, y_var_new, hctiefe_new, tiefe_new, nkzs_old,  &
+                             nkzs_new, i_zaehlv, itags, monats, mstr, ior)
+   implicit none
    
-   real, dimension(50) :: tiefe_neu, hctiefe_neu, y_var, y_var_neu
+   ! --- dummy arguments ---
+   real,    intent(in),  dimension(50) :: y_var
+   real,    intent(out), dimension(50) :: y_var_new
+   real,    intent(in),  dimension(50) :: hctiefe_new
+   real,    intent(in),  dimension(50) :: tiefe_new
+   integer, intent(in)                 :: nkzs_old
+   integer, intent(in)                 :: nkzs_new
+   integer, intent(in)                 :: i_zaehlv ! TODO: unused
+   integer, intent(in)                 :: itags    ! TODO: unused
+   integer, intent(in)                 :: monats   ! TODO: unused
+   integer, intent(in)                 :: mstr     ! TODO: unused
+   integer, intent(in)                 :: ior      ! TODO: unused
+   
+   ! --- local variables ---
+   integer :: i_neu, nkz_alt, nkz
+   real    :: yachs, stg
    
    i_neu = 1
-   do nkz_alt = 1,nkzs_alt-1
-      do nkz = i_neu,nkzs_neu
-         if (nkz == 1 .and. tiefe_neu(nkz) > hcTiefe_neu(nkz_alt)) then
-            y_var_neu(nkz) = y_var(nkz_alt)
+   do nkz_alt = 1,nkzs_old-1
+      do nkz = i_neu,nkzs_new
+         if (nkz == 1 .and. tiefe_new(nkz) > hctiefe_new(nkz_alt)) then
+            y_var_new(nkz) = y_var(nkz_alt)
             cycle
          endif
-         if (nkz == nkzs_neu .and. Tiefe_neu(nkz) < hctiefe_neu(nkzs_alt)) then
-            y_var_neu(nkz) = y_var(nkzs_alt)
+        
+         if (nkz == nkzs_new .and. tiefe_new(nkz) < hctiefe_new(nkzs_old)) then
+            y_var_new(nkz) = y_var(nkzs_old)
             exit
          endif
          
-         if (tiefe_neu(nkz) <= hcTiefe_neu(nkz_alt) .and. Tiefe_neu(nkz)>=hcTiefe_neu(nkz_alt+1)) then
-            Stg = (y_var(nkz_alt+1)-y_var(nkz_alt))/(hcTiefe_neu(nkz_alt+1)-hcTiefe_neu(nkz_alt))
-            Yachs = y_var(nkz_alt)-Stg*hcTiefe_neu(nkz_alt)
-            y_var_neu(nkz) = Stg*Tiefe_neu(nkz)+Yachs
+         if (tiefe_new(nkz) <= hctiefe_new(nkz_alt) .and. tiefe_new(nkz)>=hctiefe_new(nkz_alt+1)) then
+            Stg = (y_var(nkz_alt+1)-y_var(nkz_alt))/(hctiefe_new(nkz_alt+1)-hctiefe_new(nkz_alt))
+            Yachs = y_var(nkz_alt)-Stg*hctiefe_new(nkz_alt)
+            y_var_new(nkz) = Stg*tiefe_new(nkz)+Yachs
             cycle
          else
             i_neu = nkz
